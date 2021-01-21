f8f744c1721e46d278c1e594e4d1e6afd134ca9a
woo00oo <96woo94@naver.com>
2021-01-21 22:41:35 +0900

    Post 레파지토리 구현

--- src/main/java/com/woo00oo/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/woo00oo/book/springboot/domain/posts/PostsRepository.java
@@ -1,2 +1,6 @@
-package com.woo00oo.book.springboot.domain.posts;public class PostsRepository {
+package com.woo00oo.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
 }
