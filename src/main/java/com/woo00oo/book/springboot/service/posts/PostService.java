22797b16046f14e4426e6184c5fb24f27f128a5c
woo00oo <96woo94@naver.com>
2021-01-21 22:42:17 +0900

    Post 서비스 (저장API) 구현

--- src/main/java/com/woo00oo/book/springboot/service/posts/PostService.java
+++ src/main/java/com/woo00oo/book/springboot/service/posts/PostService.java
@@ -1,2 +1,20 @@
-package com.woo00oo.book.springboot.service.posts;public class PostService {
+package com.woo00oo.book.springboot.service.posts;
+
+import com.woo00oo.book.springboot.domain.posts.PostsRepository;
+import com.woo00oo.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
