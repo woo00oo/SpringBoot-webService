482acb60aa100dac08cc041ccef3d7f0acdbb092
woo00oo <96woo94@naver.com>
2021-01-21 22:41:53 +0900

    Post 컨트롤러 ( 저장API) 구현

--- src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
+++ src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
@@ -1,2 +1,21 @@
-package com.woo00oo.book.springboot.web;public class PostsApiController {
+package com.woo00oo.book.springboot.web;
+
+import com.woo00oo.book.springboot.service.posts.PostService;
+import com.woo00oo.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostService postService;
+
+    @PostMapping("api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto
+                     requestDto){
+        return postService.save(requestDto);
+    }
 }
