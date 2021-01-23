240dd0d8ee5d0578e5956f6bb4761a761583abfc
woo00oo <96woo94@naver.com>
2021-01-23 21:18:19 +0900

    Posts 수정 및 조회 API - 컨트롤러 계층 구현

--- src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
+++ src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.woo00oo.book.springboot.web;
 
 import com.woo00oo.book.springboot.service.posts.PostService;
+import com.woo00oo.book.springboot.web.dto.PostsResponseDto;
 import com.woo00oo.book.springboot.web.dto.PostsSaveRequestDto;
+import com.woo00oo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,9 +13,19 @@ public class PostsApiController {
 
     private final PostService postService;
 
-    @PostMapping("api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto
                      requestDto){
         return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findByid(@PathVariable Long id){
+        return postService.findById(id);
     }
 }
