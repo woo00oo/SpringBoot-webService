faba5b87a57f2a17dded16c6da640356382f8b32
woo00oo <96woo94@naver.com>
2021-02-04 21:18:20 +0900

    Posts API 컨트롤러 삭제 메소드 구현

--- src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
+++ src/main/java/com/woo00oo/book/springboot/web/PostsApiController.java
@@ -1,6 +1,6 @@
 package com.woo00oo.book.springboot.web;
 
-import com.woo00oo.book.springboot.service.posts.PostService;
+import com.woo00oo.book.springboot.service.posts.PostsService;
 import com.woo00oo.book.springboot.web.dto.PostsResponseDto;
 import com.woo00oo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.woo00oo.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -11,21 +11,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PostsApiController {
 
-    private final PostService postService;
+    private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto
                      requestDto){
-        return postService.save(requestDto);
+        return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
-        return postService.update(id, requestDto);
+        return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findByid(@PathVariable Long id){
-        return postService.findById(id);
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
