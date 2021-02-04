6b1b8d9e0963cac93e085dbda350fb5cf2bac283
woo00oo <96woo94@naver.com>
2021-02-04 20:18:39 +0900

    기본 페이지 만들기

--- src/main/java/com/woo00oo/book/springboot/web/IndexController.java
+++ src/main/java/com/woo00oo/book/springboot/web/IndexController.java
@@ -1,2 +1,13 @@
-package com.woo00oo.book.springboot.web;public class IndexController {
+package com.woo00oo.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
