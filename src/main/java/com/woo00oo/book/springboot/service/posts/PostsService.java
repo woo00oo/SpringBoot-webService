cd54e159404d768ca82894176b76dc53a53794fb
woo00oo <96woo94@naver.com>
2021-02-04 21:16:21 +0900

    index.js - 게시글 등록, 수정, 조회, 삭제 기능

--- src/main/java/com/woo00oo/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/woo00oo/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.woo00oo.book.springboot.service.posts;
 
 import com.woo00oo.book.springboot.domain.posts.Posts;
 import com.woo00oo.book.springboot.domain.posts.PostsRepository;
+import com.woo00oo.book.springboot.web.dto.PostsListResponseDto;
 import com.woo00oo.book.springboot.web.dto.PostsResponseDto;
 import com.woo00oo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.woo00oo.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,5 +39,12 @@ public class PostService {
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
